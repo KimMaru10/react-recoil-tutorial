09dae79ff32e7823165512f33d39f1df1ff0c4cd
김대성 <daesung3725@naver.com>
2024-08-08 14:31:48 +0900

    feat : 프로젝트 첫 커밋

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import InputTask from "./components/InputTask";
+import AddTask from "./components/AddTask";
+function App() {
+  return (
+    <div className="task">
+      <InputTask />
+      <AddTask />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/InputTask.tsx
@@ -0,0 +1,10 @@
+const InputTask = () => {
+  return (
+    <div className="inputField">
+      <input type="text" className="inputTitle" />
+      <button type="button">추가</button>
+    </div>
+  );
+};
+
+export default InputTask;
